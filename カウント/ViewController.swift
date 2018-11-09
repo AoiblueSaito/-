95ffdde9d61541fee671067a8b96d643ca7533a3
Aoi Saito <aoisaito@aoinomacbook-ea.local>
2018-11-09 18:56:32 +0900

    Initial Commit

---
+++ カウント/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  カウント
+//
+//  Created by Aoi Saito on 2018/11/09.
+//  Copyright © 2018年 Aoi Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
